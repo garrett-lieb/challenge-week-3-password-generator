c8de38cf705afe7b9796068810e3c4ac0e50f66c
garrett lieb <liebg08@gmail.com>
2023-11-13 16:55:34 -0800

    added confirm prompts for cap letters, low letters, numbers, and spec char in java; getting undefined message in target field

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 //variables
-var input = [""]
+var inputs = [""]
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowercase = "abcdefghiklmnopqrsttuvwxtz"
 var numbers = "0123456789"
-var special = "*&^%$#@!?><{}"
+var specialchars = "*&^%$#@!?><{}"
 var password = ""
 
 // Add event listener to generate button; moved above write password to input function.
@@ -21,10 +21,61 @@ function writePassword() {
 
 }
 
+//check length
 function generatePassword () {
   var passwordLength = prompt("Enter desired number of characters (8-128):")
 
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    alert("Invalid entry - Please enter a number between 8 and 128");
+    alert("Invalid Entry - enter a number between 8 and 128");
     return
+
+    const confirmLength = parseInt(passwordLength)
+}
+
+//confirm upper case
+var uppercaseConfirm = confirm("Include uppercase letters?");
+if (uppercaseConfirm === true) {
+  for (var i = 0; i < uppercase.length; i++) {
+    inputs.push(uppercase [i]);
+  }
+  console.log(inputs)
+}
+
+// confirm lower case
+var lowercaseConfirm = confirm("Include lowercase letters?");
+if (lowercaseConfirm === true) {
+  for (var i = 0; i < lowercase.length; i++) {
+    inputs.push(lowercase [i]);
+  }
+  console.log(inputs)
+}
+
+// confirm numbers 
+var numbersConfirm = confirm("Include numbers?");
+if (numbersConfirm === true) {
+  for (var i = 0; i < numbers.length; i++) {
+    inputs.push(numbers [i]);
+  }
+  console.log(inputs)
+}
+
+// confirm spec characters
+var specialcharsConfirm = confirm("Include special characters?");
+if (specialcharsConfirm === true) {
+  for (var i = 0; i < specialchars.length; i++) {
+    inputs.push(specialchars [i]);
+  }
+  console.log(inputs)
+}
+
+// confirm user selected at least one char type
+if (uppercaseConfirm === false && numberConfirm === false && lowercaseConfirm === false && specialcharsConfirm === false) {
+  alert("Please choose at least 1 type of character.")
+  return;
+}
+
+var password = "";
+
+
+
 }
